ddc6c1e302162b3d610aa12c4b4977a46e17174c
Simon Unge <sunge@cisco.com>
2017-10-26 13:43:23 +0200

    my stuff

---
+++ custom/alias.zsh
@@ -0,0 +1,5 @@
+alias e='emacsclient -t'
+alias apt-get='sudo apt-get'
+alias j=jump
+alias open=xdg-open
+alias te='emacsclient -n'
---
+++ custom/tailf.zsh
@@ -0,0 +1,40 @@
+rt() {
+    if [ $# -eq 0 ]; then
+        cd ~/work/rt
+    else
+        local dir=~/work/rt/${1}
+        if [ ! -d ${dir} ]; then
+            mkdir ${dir}
+            cp -r ~/work/clean_confd/* ${dir}
+        else
+            echo "RT directory ${1} already existing, just going there..."
+        fi
+        cd ${dir}
+        (get_attachments ${1} &) &> /dev/null
+    fi
+}
+
+rtn() {
+    if [ $# -eq 0 ]; then
+        cd ~/work/rt
+    else
+        local dir=~/work/rt/${1}
+        if [ ! -d ${dir} ]; then
+            mkdir ${dir}
+            cp -r ~/work/clean_ncs/* ${dir}
+        else
+            echo "RT directory ${1} already existing, just going there..."
+        fi
+        cd ${dir}
+        (get_attachments ${1} &) &> /dev/null
+    fi
+}
+
+yang() {
+    local name="$(shuf -n 1 ~/work/scripts/yangvillainnames).yang"
+    if [ -e "./${name}" ]; then
+        yang
+    else
+        emacsclient -n $name
+    fi
+}
